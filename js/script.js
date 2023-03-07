4b378ebed4c9ece772ed25d2a6e7c4ee348537c7
chowdhuryjoy <126226436+chowdhuryjoy@users.noreply.github.com>
2023-03-07 17:37:48 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,25 @@
+$(function(){
+
+  // BANNER SLIDER
+  $('.banner-text-wrapper').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 1500,
+    arrows: false,
+    pauseOnHover: false,
+    dots: true,
+  });
+
+  // TESTIMONIAL SLIDER
+  $('.test-slider').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 1500,
+    arrows: false,
+    pauseOnHover: false,
+    dots: true,
+  });
+
+})
